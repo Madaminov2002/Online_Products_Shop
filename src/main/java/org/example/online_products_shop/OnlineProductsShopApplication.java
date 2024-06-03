e10cb578815860e2f1085178bac713431fbf2380
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-06-03 10:34:09 +0500

    first commit

---
+++ src/main/java/org/example/online_products_shop/OnlineProductsShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.online_products_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineProductsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineProductsShopApplication.class, args);
+    }
+
+}
