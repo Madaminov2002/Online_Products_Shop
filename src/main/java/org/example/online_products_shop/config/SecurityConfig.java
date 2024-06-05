1d23da7cf584df6ac0090026af8138b3ce509041
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-06-05 17:08:38 +0500

    UserDetailsServiceAutoConfiguration is turned off

--- src/main/java/org/example/online_products_shop/config/SecurityConfig.java
+++ src/main/java/org/example/online_products_shop/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/swagger-ui/**",
             "/v3/api-docs/**",
-            "/swagger-resources/**"
+            "/swagger-resources/**",
+            "/swagger"
     };
 
 
